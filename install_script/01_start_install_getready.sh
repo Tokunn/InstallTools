bd6c54ae957145f5e832520da238325cecfac432
Tokunn <you@example.com>
2015-06-15 03:58:36 +0900

    update

--- install_script/01_start_install_getready.sh
+++ install_script/01_start_install_getready.sh
@@ -2,7 +2,7 @@
 
 systemctl enable dhcpcd.service
 
-pacman -Syu --noconfirm
-pacman -S git python2 python3 ipython3 --noconfirm
+pacman -Syu --noconfirm --needed
+pacman -S git python2 python3 ipython3 --noconfirm --needed
 
 ./02_start_install.py
