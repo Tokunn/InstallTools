09ed1b6785fb835c4f827c18c7d12ea1b20ff2f0
Tokunn <you@example.com>
2015-06-15 01:37:11 +0900

    update

---
+++ sync_files.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env ipython3
+# -*- coding: utf-8 -*-
+
+import os, sys
+import subprocess
+
+
+def main():
+    print("\n###rc.lua")
+    get_ipython().system('mkdir -p /home/tokunn/.config/awesome')
+    get_ipython().system('cp /home/tokunn/Program/InstallTools/awesome/rc.lua /home/tokunn/.config/awesome/rc.lua')
+
+    print("\n###vimperatorrc")
+    get_ipython().system('cp /home/tokunn/Program/InstallTools/vimperator/.vimperatorrc /home/tokunn/.vimperatorrc')
+
+    print("\n###conkyrc")
+    get_ipython().system('cp /home/tokunn/Program/InstallTools/conky/.conkyrc /home/tokunn/.conkyrc')
+
+    print("\n###vimrc")
+    get_ipython().system('cp /home/tokunn/Program/InstallTools/vim/.vimrc /home/tokunn/.vimrc')
+
+    print("\n###vim/template")
+    get_ipython().system('mkdir -p /home/tokunn/.vim')
+    get_ipython().system('cp -r /home/tokunn/Program/InstallTools/vim/.vim/template /home/tokunn/.vim/template')
+
+    print("\n###xinitrc")
+    get_ipython().system('cp /home/tokunn/Program/InstallTools/xorg/.xinitrc /home/tokunn/.xinitrc')
+
+    print("\n###zshrc")
+    get_ipython().system('cp /home/tokunn/Program/InstallTools/zsh/.zshrc /home/tokunn/.zshrc')
+
+if __name__ == '__main__':
+    main()
